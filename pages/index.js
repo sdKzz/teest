f40649f99d866713ee1602213e3158e33c5afd16
sdKzz <140256302+sdKzz@users.noreply.github.com>
2023-08-07 18:43:56 +0000

    alteração mae

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Gabriela, eu amo você. Se você me ama, dá uma risadinha! Manda aquilo que te pedi no wpp</h1>
+    return <h1>Mãezinha, eu te amo! A senhora poderia fazer um pix?</h1>
 }
 
 export default Home; 
